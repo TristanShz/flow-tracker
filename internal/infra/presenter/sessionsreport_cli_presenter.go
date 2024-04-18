b3f361fc27943e756f43dbb79fd7f274fd71bf62
Tristan Schwartz <tristan@lesentrecodeurs.com>
2024-04-18 07:02:27 +0200

    wip: sessions report CLI presenter

--- internal/infra/presenter/sessionsreport_cli_presenter.go
+++ internal/infra/presenter/sessionsreport_cli_presenter.go
@@ -1,8 +1,28 @@
 package presenter
 
-import "github.com/TristanSch1/flow/internal/domain/sessionsreport"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/TristanSch1/flow/internal/domain/sessionsreport"
+	"github.com/TristanSch1/flow/utils"
+)
 
 type SessionsReportCLIPresenter struct{}
 
 func (s SessionsReportCLIPresenter) Show(sessionsReport sessionsreport.SessionsReport) {
+	text := "Sessions Report\n\n"
+
+	for _, session := range sessionsReport.Sessions {
+		text += fmt.Sprintf(
+			"    From %v to %v %v %v [%v]\n",
+			utils.GreenText(session.StartTime.Format("15:04:05")),
+			utils.GreenText(session.EndTime.Format("15:04:05")),
+			session.Duration().String(),
+			utils.PurpleText(session.Project),
+			utils.YellowText(strings.Join(session.Tags, ", ")),
+		)
+	}
+
+	fmt.Println(text)
 }
