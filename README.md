81b07cc2b04abbd0cd90a46a18fa99b447d06ec8
Tristan Schwartz <tristan@lesentrecodeurs.com>
2024-04-20 15:37:55 +0200

    doc: start readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# flow
+# Flow
+
+Flow is a CLI tool that helps you manage your time spent developing your projects. It is designed to be simple, fast and easy to use.
+
+It is written in GO and uses File system to store the data.
+
+The project is in its early stages and is still under development.
+
+## Roadmap
+
+- [x] Start a flow session
+- [x] Stop a flow session
+- [x] View current session status
+- [x] View a report of all sessions
+- [x] View a report of all sessions for a given project
+- [x] List all projects
+- [ ] View a report of sessions in a given time range
+- [ ] Edit a session
+- [ ] Export report to CSV
+- [ ] Export report to JSON
--- internal/application/usecases/project/list/list_test.go
+++ internal/application/usecases/project/list/list_test.go
@@ -10,20 +10,63 @@ import (
 
 func TestListProjects_Success(t *testing.T) {
 	f := tests.GetSessionFixture(t)
+	tests := []struct {
+		name          string
+		givenSessions []session.Session
+		want          []string
+	}{
+		{
+			name: "Two projects",
+			givenSessions: []session.Session{
+				{
+					Id:        "1",
+					StartTime: time.Date(2024, time.April, 14, 10, 12, 0, 0, time.UTC),
+					EndTime:   time.Date(2024, time.April, 14, 13, 10, 0, 0, time.UTC),
+					Project:   "MyTodo",
+					Tags:      []string{"add-todo"},
+				}, {
+					Id:        "2",
+					StartTime: time.Date(2024, time.April, 14, 14, 12, 0, 0, time.UTC),
+					EndTime:   time.Date(2024, time.April, 14, 15, 12, 0, 0, time.UTC),
+					Project:   "Flow",
+					Tags:      []string{"start-usecase"},
+				},
+			},
+			want: []string{"MyTodo", "Flow"},
+		},
+		{
+			name:          "No sessions",
+			givenSessions: []session.Session{},
+			want:          []string{},
+		},
+		{
+			name: "Two sessions with the same project",
+			givenSessions: []session.Session{
+				{
+					Id:        "1",
+					StartTime: time.Date(2024, time.April, 14, 10, 12, 0, 0, time.UTC),
+					EndTime:   time.Date(2024, time.April, 14, 13, 10, 0, 0, time.UTC),
+					Project:   "Flow",
+					Tags:      []string{"start-usecase"},
+				}, {
+					Id:        "2",
+					StartTime: time.Date(2024, time.April, 14, 14, 12, 0, 0, time.UTC),
+					EndTime:   time.Date(2024, time.April, 14, 15, 12, 0, 0, time.UTC),
+					Project:   "Flow",
+					Tags:      []string{"start-usecase"},
+				},
+			},
+			want: []string{"Flow"},
+		},
+	}
 
-	f.GivenSomeSessions([]session.Session{{
-		StartTime: time.Date(2024, time.April, 14, 10, 12, 0, 0, time.UTC),
-		EndTime:   time.Date(2024, time.April, 14, 13, 10, 0, 0, time.UTC),
-		Project:   "MyTodo",
-		Tags:      []string{"add-todo"},
-	}, {
-		StartTime: time.Date(2024, time.April, 14, 14, 12, 0, 0, time.UTC),
-		EndTime:   time.Date(2024, time.April, 14, 15, 12, 0, 0, time.UTC),
-		Project:   "Flow",
-		Tags:      []string{"start-usecase"},
-	}})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f.GivenSomeSessions(tt.givenSessions)
 
-	f.WhenGettingListOfProjects()
+			f.WhenGettingListOfProjects()
 
-	f.ThenProjectsShouldBe([]string{"MyTodo", "Flow"})
+			f.ThenProjectsShouldBe(tt.want)
+		})
+	}
 }
