ee989b620886bf3113ffc0e1fa141e14f7a2cff5
Tristan Schwartz <tristan@lesentrecodeurs.com>
2024-04-23 20:55:34 +0200

    test: add tests for timeRange functions

--- pkg/timerange/time_range_test.go
+++ pkg/timerange/time_range_test.go
@@ -7,6 +7,78 @@ import (
 	"github.com/TristanSch1/flow/pkg/timerange"
 )
 
+func TestTimeRange(t *testing.T) {
+	tests := []struct {
+		tr                timerange.TimeRange
+		name              string
+		wantIsZero        bool
+		wantJustSince     bool
+		wantJustUntil     bool
+		wantSinceAndUntil bool
+	}{
+		{
+			name: "Since is zero",
+			tr: timerange.TimeRange{
+				Since: time.Time{},
+				Until: time.Date(2024, 4, 17, 20, 0, 0, 0, time.UTC),
+			},
+			wantIsZero:        false,
+			wantJustSince:     false,
+			wantJustUntil:     true,
+			wantSinceAndUntil: false,
+		},
+		{
+			name: "Until is zero",
+			tr: timerange.TimeRange{
+				Since: time.Date(2024, 4, 17, 20, 0, 0, 0, time.UTC),
+				Until: time.Time{},
+			},
+			wantIsZero:        false,
+			wantJustSince:     true,
+			wantJustUntil:     false,
+			wantSinceAndUntil: false,
+		},
+		{
+			name: "Both are zero",
+			tr: timerange.TimeRange{
+				Since: time.Time{},
+				Until: time.Time{},
+			},
+			wantIsZero:        true,
+			wantJustSince:     false,
+			wantJustUntil:     false,
+			wantSinceAndUntil: false,
+		},
+		{
+			name: "None are zero",
+			tr: timerange.TimeRange{
+				Since: time.Date(2024, 4, 17, 20, 0, 0, 0, time.UTC),
+				Until: time.Date(2024, 4, 17, 20, 0, 0, 0, time.UTC),
+			},
+			wantIsZero:        false,
+			wantJustSince:     false,
+			wantJustUntil:     false,
+			wantSinceAndUntil: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.tr.IsZero(); got != tt.wantIsZero {
+				t.Errorf("TimeRange.IsZero() = %v, want %v", got, tt.wantIsZero)
+			}
+
+			if got := tt.tr.JustSince(); got != tt.wantJustSince {
+				t.Errorf("TimeRange.JustSince() = %v, want %v", got, tt.wantJustSince)
+			}
+
+			if got := tt.tr.JustUntil(); got != tt.wantJustUntil {
+				t.Errorf("TimeRange.JustUntil() = %v, want %v", got, tt.wantJustUntil)
+			}
+		})
+	}
+}
+
 func TestTimeRange_NewDayTimeRange(t *testing.T) {
 	day := time.Date(2024, 4, 17, 19, 0, 0, 0, time.UTC)
 	expected := timerange.TimeRange{
