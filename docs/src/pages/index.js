338bf27f40e66a893d8ddad1b14d0624d28d5a4d
Tristan Schwartz <tristan@lesentrecodeurs.com>
2024-05-04 08:26:52 +0200

    docs: meta

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -32,10 +32,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={`${siteConfig.title} - Documentation`} description="">
       <HomepageHeader />
       <main></main>
     </Layout>
